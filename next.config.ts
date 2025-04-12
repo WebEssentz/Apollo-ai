06b13c26b8b9388de8044793ca1ccb91813bf7a3
nebulacommander <godwinonyerikam@gmail.com>
2025-04-12 08:03:21 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,16 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['firebasestorage.googleapis.com']
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Also ignore TypeScript errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
